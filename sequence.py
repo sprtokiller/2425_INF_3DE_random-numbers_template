be6a5d030b8cafdbdf77b6538d8fc532a427514d
3.E Zatko Dominik <Dominik.Zatko@student.sps-cl.cz>
2024-10-07 11:39:55 +0200

    sequence

--- sequence.py
+++ sequence.py
@@ -1,8 +1,10 @@
 import random
 
-def generate_sequence(n):
-    # Tvůj kód zde
+def generate_sequence(number):
+    for _ in range(number):
+        print(random.randrange(100))
     pass
+    
 
 # Otestování funkce
 generate_sequence(10)  # Vygeneruje 10 náhodných čísel
