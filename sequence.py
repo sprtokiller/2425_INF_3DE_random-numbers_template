4298d3e974f679aa7a4882087172b6d778c16d0d
jakubsps6 <jakub.zeman@student.sps-cl.cz>
2024-10-07 12:07:09 +0200

    sequence

--- sequence.py
+++ sequence.py
@@ -2,7 +2,8 @@ import random
 
 def generate_sequence(n):
     # Tvůj kód zde
-    pass
+    for _ in range(n):
+        print(random.randint(0, 9))
 
 # Otestování funkce
 generate_sequence(10)  # Vygeneruje 10 náhodných čísel
