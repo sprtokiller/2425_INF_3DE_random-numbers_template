6f3b0c3ed5073432f3203bd2aa3239416358e1a8
3.E Weich Jakub <Jakub.Weich@student.sps-cl.cz>
2024-10-07 12:25:45 +0200

    random generators + schedule generator

--- biased_coin.py
+++ biased_coin.py
@@ -2,7 +2,8 @@ import random
 
 def flip_biased_coin(p):
     # Tvůj kód zde
-    pass
+    coinflip = ["heads", "tails"];
+    return random.choice(coinflip);
 
 # Otestování funkce
 print(flip_biased_coin(0.7))  # Simulace hodu s 70% šancí na výhru
