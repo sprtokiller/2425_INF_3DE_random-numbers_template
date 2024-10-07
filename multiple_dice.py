67ecc3c1cd083a4db9ff41a64af18739c3d5f21c
jakubsps6 <jakub.zeman@student.sps-cl.cz>
2024-10-07 11:51:30 +0200

    multiple_dice

--- multiple_dice.py
+++ multiple_dice.py
@@ -2,7 +2,10 @@ import random
 
 def roll_multiple_dice(n, k):
     # Tvůj kód zde
-    pass
+    i = 0
+    while i < n:
+        return random.randint(0, k)
+        i += 1
 
 # Otestování funkce
 print(roll_multiple_dice(3, 6))  # Simulace hodu 3 kostkami, každá má 6 hran
