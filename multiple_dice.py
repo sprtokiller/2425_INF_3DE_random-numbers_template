3d2b89f8f7dc91fc611042c40cc641f0eeac2491
Leona Černá <leona.cerna@student.sps-cl.cz>
2024-10-07 12:05:02 +0200

    Vše hotovo

--- multiple_dice.py
+++ multiple_dice.py
@@ -1,9 +1,9 @@
 import random
 
 def roll_multiple_dice(n, k):
-    for _ in range(1, n):
-        random.randint(1, k)
+    for _ in range(n):
+        print(random.randint(0, k))
     return
 
 # Otestování funkce
-print(roll_multiple_dice(3, 6))  # Simulace hodu 3 kostkami, každá má 6 hran
+roll_multiple_dice(3, 6) # Simulace hodu 3 kostkami, každá má 6 hran
